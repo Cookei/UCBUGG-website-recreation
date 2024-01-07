06f190cc454febf5dfb641326adf66b0dbbb011e
Cookie <hanlicookie@outlook.com>
2024-01-06 23:18:58 -0800

    navbar

--- ucbugg-site/src/App.js
+++ ucbugg-site/src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import ReactDom from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Syllabus from './pages/Syllabus';
+import Labs from "./pages/Labs";
+import Projects from "./pages/Projects";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Navbar></Navbar>
         <Routes>
           <Route index element={<Home/>}/>
-          <Route path="about" element={<About/>}/>
-          <Route path="syllabus" element={<Syllabus/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/syllabus" element={<Syllabus/>}/>
+          <Route path="/labs" element={<Labs/>}/>
+          <Route path="/projects" element={<Projects/>}/>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ucbugg-site/src/pages/Labs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Labs = () => {
+  return (
+    <div>Labs</div>
+  )
+}
+
+export default Labs
