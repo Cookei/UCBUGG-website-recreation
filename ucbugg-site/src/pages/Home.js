12d8073325bb16e5800ba6f496ec9a785559f25b
Cookie <hanlicookie@outlook.com>
2024-01-08 23:04:53 -0800

    splash text

--- ucbugg-site/src/pages/Home.js
+++ ucbugg-site/src/pages/Home.js
@@ -5,9 +5,20 @@ const Home = forwardRef((props, ref) => {
   return (
     <div>
       <section>
-        <div>
+        <div className={styles.splashSection}>
           <div ref={ref} className={styles.splashView}></div>
-          <div>Home</div>
+          <div id={styles.splashTitle}>
+            <h1>UCBUGG</h1>
+            <h2>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut
+              quam felis. Nunc malesuada sodales vulputate. Ut eu risus a quam
+              hendrerit pharetra. Duis elementum augue eget dictum iaculis.
+              Aenean vitae ex ut velit condimentum interdum eget a justo.
+              Vivamus fringilla enim hendrerit eros mattis bibendum. Sed
+              accumsan lectus justo, ullamcorper scelerisque turpis tincidunt
+              ac. Nulla ut hendrerit nulla, in venenatis tortor.
+            </h2>
+          </div>
         </div>
       </section>
     </div>
